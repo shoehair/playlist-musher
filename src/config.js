cbd36f9f9634c9a99b6a18fa5b672e739a3a4417
Isabella Borkovic <iborkovic@berkeley.edu>
2022-01-10 23:04:50 -0800

    correct redirect link

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "2de86c0831c247238aee649cc1b0abf8";
-export const redirectUri = "http://localhost:3000/redirect";
-//export const redirectUri = "https://playlist-musher.herokuapp.com/";
+//export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "https://playlist-musher.herokuapp.com/";
 
 export const scopes = [
     "user-top-read",
